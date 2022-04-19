bd3d7f29bf62ae24e562d62c57e9e4bdeea5fd7c
mattbooker <mattbooker97@gmail.com>
2022-04-19 17:37:04 -0400

    WIP: Frame class update

--- svo/frame.py
+++ svo/frame.py
@@ -1,4 +1,22 @@
+import numpy as np
+
+frame_number = 0
+
 class Frame:
 
-    def __init__(self):
-        self.features = None
+    def __init__(self, image):
+        self.id = frame_number
+        self.image_ = image
+        self.keypoints_ = None
+        self.kp_scores_ = None
+        self.is_keyframe_ = False
+
+        # Transform from world to frame
+        self.T_w_f_ = np.eye(4)
+
+        frame_number += 1
+
+    def setKeypoints(self, keypoints, scores):
+        self.keypoints_ = keypoints
+        self.scores = scores
+
