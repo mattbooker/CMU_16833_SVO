cd22728e06098ba38869d63fbed5a19fe3e7c1bc
manujtrehan <trehan.manuj@gmail.com>
2022-04-23 18:52:03 -0400

    Added the function to check if a frame is a keyframe or not

--- svo/map.py
+++ svo/map.py
@@ -10,3 +10,24 @@ class Map:
     def initial_map(self):
         # triangulate initial map from first two views
         pass
+
+    def checkKeyframe(self, frame):
+        total_dist = 0
+        max_dist = 0
+        for idx, keyframe in enumerate(self.keyframes):
+            # Calculate distance between the frame and the keyframe
+            T_frame = keyframe.T_w_f_ @ np.inv(frame.T_w_f_)
+            dist = np.linalg.norm(T_frame[:-1, -1])
+            if dist > max_dist:
+                max_dist = dist
+                max_index = idx
+            total_dist += dist
+        
+        avg_dist = total_dist/len(self.keyframes)
+
+        if avg_dist > 0.12 * self.avg_scene_depth:
+            # frame is a keyframe
+            frame.is_keyframe_ = True
+            # remove farthest keyframe, and add new keyframe to map list of keyframes 
+            del self.keyframes[max_index]
+            self.keyframes.append(frame)
